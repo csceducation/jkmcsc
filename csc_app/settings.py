3d45d837c7a0f2a84604f65b93440d61e55a15e9
Senthilsk10 <132420106+Senthilsk10@users.noreply.github.com>
2025-03-31 12:27:28 +0530

    Update settings.py

--- csc_app/settings.py
+++ csc_app/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = "csc_app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
+
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
@@ -114,6 +114,7 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+'''
 
 
 # Password validation
